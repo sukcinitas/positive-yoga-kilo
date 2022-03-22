82108a9c5750c8fa21263c36086bafcaa2e9ef52
Gintare Cerniauskaite <gintare.cerniauskaite@protonmail.com>
2022-03-22 18:45:06 +0200

    Add Stars component

---
+++ src/components/Stars.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { StyledStarIcon, StyledOutlineStarIcon } from "../shared/MaterialIcons";
+
+function Stars() {
+  return (
+    <div>
+      <StyledStarIcon></StyledStarIcon>
+      <StyledStarIcon></StyledStarIcon>
+      <StyledStarIcon></StyledStarIcon>
+      <StyledOutlineStarIcon></StyledOutlineStarIcon>
+      <StyledOutlineStarIcon></StyledOutlineStarIcon>
+    </div>
+  );
+}
+
+export default Stars;
